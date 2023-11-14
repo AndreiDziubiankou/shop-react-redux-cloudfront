a5c62b145ff83b53a84b48ca795d1cbbfdf89b08
EPAM\Andrei_Dziubiankou <andrei_dziubiankou@epam.com>
2023-11-14 11:07:38 +0200

    add authorization checking

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import axios from "axios";
+import axios, { AxiosRequestHeaders } from "axios";
 
 type CSVFileImportProps = {
   url: string;
@@ -26,6 +26,13 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const uploadFile = async () => {
     console.log("uploadFile to", url);
 
+    const authorization_token = localStorage.getItem("authorization_token");
+    const headers: AxiosRequestHeaders = {};
+
+    if (authorization_token) {
+      headers.Authorization = `Basic ${authorization_token}`;
+    }
+
     // Get the presigned URL
     const response = await axios({
       method: "GET",
@@ -33,6 +40,7 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
       params: {
         name: encodeURIComponent(file ? file.name : ""),
       },
+      headers,
     });
 
     console.log("File to upload: ", file ? file.name : "");
